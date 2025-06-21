1e247635f31d3a0f826c42dae3119510e860324d
Yevgenii <yevgenii.lobanov@gmail.com>
2025-06-21 22:53:09 +0400

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test-2',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=test-2',
     'username' => 'test',
     'password' => 'test',
     'charset' => 'utf8',
